afec6d0b23546661ecd2879ad40a72e5d2abebe2
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-10 13:34:01 +0330

    Add project files.

---
+++ Weather/MVVM/Models/WeatherData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Weather.MVVM.Models
+{
+
+    public class WeatherData
+     {
+          public float latitude { get; set; }
+          public float longitude { get; set; }
+          public float generationtime_ms { get; set; }
+          public int utc_offset_seconds { get; set; }
+          public float elevation { get; set; }
+          public Current_Weather current_weather { get; set; }
+          public Daily_Units daily_units { get; set; }
+          public Daily daily { get; set; }
+          public ObservableCollection<Daily2> daily2 { get; set; } =
+               new ObservableCollection<Daily2>();
+     }
+
+     public class Current_Weather
+     {
+          public float temperature { get; set; }
+          public float windspeed { get; set; }
+          public float winddirection { get; set; }
+          public float weathercode { get; set; }
+          public string time { get; set; }
+     }
+
+     public class Daily_Units
+     {
+          public string time { get; set; }
+          public string weathercode { get; set; }
+          public string temperature_2m_max { get; set; }
+          public string temperature_2m_min { get; set; }
+     }
+
+     public class Daily
+     {
+          public string[] time { get; set; }
+          public float[] weathercode { get; set; }
+          public float[] temperature_2m_max { get; set; }
+          public float[] temperature_2m_min { get; set; }
+     }
+
+     public class Daily2
+     {
+          public string time { get; set; }
+          public float weathercode { get; set; }
+          public float temperature_2m_max { get; set; }
+          public float temperature_2m_min { get; set; }
+     }
+
+
+}
---
+++ Weather/MVVM/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,86 @@
+﻿using PropertyChanged;
+using System.Text.Json;
+using System.Windows.Input;
+using Weather.MVVM.Models;
+
+namespace Weather.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+     public class WeatherViewModel
+     {
+
+          public WeatherData WeatherData { get; set; }
+          public string PlaceName { get; set; }
+          public DateTime Date { get; set; } =
+               DateTime.Now;
+
+          public bool IsVisible { get; set; }
+          public bool IsLoading { get; set; }
+
+          private HttpClient client;
+
+          public WeatherViewModel()
+          {
+               client = new HttpClient();
+          }
+
+          public ICommand SearchCommand =>
+               new Command(async (searchText) =>
+               {
+                    PlaceName = searchText.ToString();
+                    var location =
+                         await GetCoordinatesAsync(searchText.ToString());
+                    await GetWeather(location);
+               });
+
+
+          private async Task GetWeather(Location location)
+          {
+               var url =
+                    $"https://api.open-meteo.com/v1/forecast?latitude={location.Latitude}&longitude={location.Longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=America%2FChicago";
+
+               IsLoading = true;
+
+               var response =
+                 await client.GetAsync(url);
+
+               if (response.IsSuccessStatusCode)
+               {
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                    {
+                         var data = await JsonSerializer
+                              .DeserializeAsync<WeatherData>(responseStream);
+                         WeatherData = data;
+
+                         for(int i = 0; i < WeatherData.daily.time.Length; i++)
+                         {
+                              var daily2 = new Daily2
+                              {
+                                   time = WeatherData.daily.time[i],
+                                   temperature_2m_max = WeatherData.daily.temperature_2m_max[i],
+                                   temperature_2m_min = WeatherData.daily.temperature_2m_min[i],
+                                   weathercode = WeatherData.daily.weathercode[i]
+                              };
+                              WeatherData.daily2.Add(daily2);
+                         }
+                         IsVisible = true;
+                    }
+               }
+               IsLoading = false;
+          }
+
+          private async Task<Location> GetCoordinatesAsync(string address)
+          {
+               IEnumerable<Location> locations = await Geocoding
+                    .Default.GetLocationsAsync(address);
+
+               Location location = locations?.FirstOrDefault();
+
+               if (location != null)
+                    Console
+                         .WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+               return location;
+          }
+
+     }
+}
